21a83a959247eef5943c9bb11e7705d10a1e8e04
202116302 <wls5258@naver.com>
2025-11-02 14:26:36 +0900

    기존 aws 데이터 수집 코드 수정

--- thingspeak_log.py
+++ thingspeak_log.py
@@ -1,36 +1,107 @@
 from datetime import datetime
-import time
-import urllib.request
-from urllib.error import URLError
+import pandas as pd
 import requests
 import os
 
+def get_aws(year, month, day):
+    api_url = f"http://203.239.47.148:8080/dspnet.aspx?Site=85&Dev=1&Year={year}&Mon={month}&Day={day}"
+    response = requests.get(api_url)
+    data = response.text
+    data = data.strip().split('\n')
+    df = pd.DataFrame([line.split(',') for line in data])
+
+    timestamp = df.iloc[:, 0]
+    temp = df.iloc[:, 1]
+    humid = df.iloc[:, 2]
+    Radn = df.iloc[:, 6]
+    wind_from = df.iloc[:, 7]
+    wind = df.iloc[:, 13]
+    rain = df.iloc[:, 14]
+    battery = df.iloc[:, 16]
+
+    return temp, humid, Radn, wind_from, wind, rain, battery, timestamp
+
+def save_to_csv(year, month, day, data):
+    filename = f'./output/'
+    if not os.path.exists(filename):
+        os.makedirs(filename)
+
+    file_path = os.path.join(filename, f'{year}_{month}.csv')
+
+    if os.path.exists(file_path):
+        existing_data = pd.read_csv(file_path)
+        new_data = data.rename(columns={0: 'Timestamp', 1: 'Temp', 2: 'Humid', 6: 'Radn', 7: 'Wind_degree', 13: 'Wind', 14: 'Rainfall', 16: 'Battery'})
+        merged_data = pd.concat([existing_data, new_data], ignore_index=True)
+        merged_data = merged_data.drop_duplicates('Timestamp')
+        merged_data.to_csv(file_path, index=False)
+    else:
+        df = data.rename(columns={0: 'Timestamp', 1: 'Temp', 2: 'Humid', 6: 'Radn', 7: 'Wind_degree', 13: 'Wind', 14: 'Rainfall', 16: 'Battery'})
+        df.to_csv(file_path, index=False)
+
+    if data.empty:
+        print("데이터가 비어있습니다.")
+
 def main():
-    x = datetime.now()
+    current_date = datetime.now()
+    year = current_date.year
+    month = str(current_date.month).zfill(2)
+    day = str(current_date.day).zfill(2)
 
-    year = x.year
-    mon = f"{x.month:02d}"
-    day = f"{x.day:02d}"
+    temp, humid, Radn, wind_from, wind, rain, battery, timestamp = get_aws(year, month, day)
+    data = pd.concat([timestamp, temp, humid, Radn, wind_from, wind, rain, battery], axis=1)
+    save_to_csv(year, month, day, data)
 
-    url = f"http://203.239.47.148:8080/dspnet.aspx?Site=85&Dev=1&Year={year}&Mon={mon}&Day={day}"
-    context = requests.get(url).text
-    data_sep = context.split("\r\n")
-
-    data_list = [x.split(',')[:-1] for x in data_sep][:-1]
-
-    last_data = data_list[-1]
-
-    del last_data[3:6]
-    del last_data[8:13]
-
-    thingspeak_key = os.environ['THINGSPEAK_KEY']
-
-    urllib.request.urlopen(
-        "https://api.thingspeak.com/update?api_key={}&field1={:0.1f}&field2={:0.1f}&field3={:0.1f}&field4={:0.1f}&field5={:0.1f}&field6={:0.1f}&field7={:0.1f}&field8={:0.1f}".format(thingspeak_key,
-            float(last_data[1]), float(last_data[2]), float(last_data[3]), float(last_data[4]), float(last_data[5]),
-            float(last_data[6]), float(last_data[7]), float(last_data[8])))
+if __name__ == '__main__':
+    main()
 
 
-#
+def get_aws(year, month, day):
+    api_url = f"http://203.239.47.148:8080/dspnet.aspx?Site=85&Dev=1&Year={year}&Mon={month}&Day={day}"
+    response = requests.get(api_url)
+    data = response.text
+    data = data.strip().split('\n')
+    df = pd.DataFrame([line.split(',') for line in data])
+
+    timestamp = df.iloc[:, 0]
+    temp = df.iloc[:, 1]
+    humid = df.iloc[:, 2]
+    Radn = df.iloc[:, 6]
+    wind_from = df.iloc[:, 7]
+    wind = df.iloc[:, 13]
+    rain = df.iloc[:, 14]
+    battery = df.iloc[:, 16]
+
+    return temp, humid, Radn, wind_from, wind, rain, battery, timestamp
+
+def save_to_csv(year, month, day, data):
+    filename = f'./output/'
+    if not os.path.exists(filename):
+        os.makedirs(filename)
+
+    file_path = os.path.join(filename, f'{year}_{month}_{day}.csv')
+
+    if os.path.exists(file_path):
+        existing_data = pd.read_csv(file_path)
+        new_data = data.rename(columns={0: 'Timestamp', 1: 'Temp', 2: 'Humid', 6: 'Radn', 7: 'Wind_degree', 13: 'Wind', 14: 'Rainfall', 16: 'Battery'})
+        merged_data = pd.concat([existing_data, new_data], ignore_index=True)
+        merged_data = merged_data.drop_duplicates('Timestamp')
+        merged_data.to_csv(file_path, index=False)
+    else:
+        df = data.rename(columns={0: 'Timestamp', 1: 'Temp', 2: 'Humid', 6: 'Radn', 7: 'Wind_degree', 13: 'Wind', 14: 'Rainfall', 16: 'Battery'})
+        df.to_csv(file_path, index=False)
+
+    if data.empty:
+        print("데이터가 비어있습니다.")
+
+def main():
+    current_date = datetime.now()
+    year = current_date.year
+    month = str(current_date.month).zfill(2)
+    day = str(current_date.day).zfill(2)
+
+    temp, humid, Radn, wind_from, wind, rain, battery, timestamp = get_aws(year, month, day)
+    data = pd.concat([timestamp, temp, humid, Radn, wind_from, wind, rain, battery], axis=1)
+    save_to_csv(year, month, day, data)
+
 if __name__ == '__main__':
     main()
